f88a2e7e50649040a4605f59a22b4cc85154a0c8
Tristyn Lai <tlai5@ucsc.edu>
2023-04-24 22:36:30 -0700

    Finished Game

--- src/main.js
+++ src/main.js
@@ -11,5 +11,5 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-//Reserve keyboard vars
+//Reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT;
--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,24 +1,23 @@
-// Rocket prefab
+//Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
-  
-      // add object to existing scene
-      scene.add.existing(this);
-      this.isFiring = false;
-      this.moveSpeed = 2;
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);   //Add to existing, displayList, updateList
+        this.isFiring = false;      //Track rocket's firing status
+        this.moveSpeed = 2;         //Pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket')  //Add rocket sfx
     }
 
-    update(){
+    update() {
         //Left/right movement
-        if(!this.isFiring){
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-            }else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
                 this.x += this.moveSpeed;
             }
         }
-
         //Fire button
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
@@ -34,5 +33,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
     }
 
-    
-  }
+    //Reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+}
